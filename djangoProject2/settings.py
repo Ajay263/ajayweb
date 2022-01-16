5409269ba1172fd0074c54c4eb03b3899a5ed1a2
Ajay263 <junioralexio607@gmail.com>
2022-01-16 15:11:16 +0200

    initial

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -16,6 +16,8 @@ import django_heroku
 import dj_database_url
 
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +131,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -145,5 +149,4 @@ EMAIL_HOST_PASSWORD = 'Nokutenda'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-STATICFILES.STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+
